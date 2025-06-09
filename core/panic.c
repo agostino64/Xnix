695395f93c88a61524bc74a7344f4594a19a649f
Agustin Gutierrez <gutierrezaverruz0@hotmail.com>
2025-06-08 18:49:11 -0600

    [core] panic: fix print error
    Signed-off-by: Agustin Gutierrez <gutierrezaverruz0@hotmail.com>

--- core/panic.c
+++ core/panic.c
@@ -33,11 +33,11 @@ void panic(registers_t* regs, const char* message)
     if (regs) {
         // Print CPU register state
         KLOG(LOG_LEVEL_INFO, "==== Registers Dump ====\n");
-        KLOG(LOG_LEVEL_INFO, " EAX: 0x%08x  EBX: 0x%08x  ECX: 0x%08x  EDX: 0x%08x\n", regs->eax, regs->ebx, regs->ecx, regs->edx);
-        KLOG(LOG_LEVEL_INFO, " ESI: 0x%08x  EDI: 0x%08x  EBP: 0x%08x  ESP: 0x%08x\n", regs->esi, regs->edi, regs->ebp, regs->esp);
-        KLOG(LOG_LEVEL_INFO, " EIP: 0x%08x  CS:  0x%08x  EFLAGS: 0x%08x\n", regs->eip, regs->cs, regs->eflags);
-        KLOG(LOG_LEVEL_INFO, " USERESP: 0x%08x  SS: 0x%08x\n", regs->useresp, regs->ss);
-        KLOG(LOG_LEVEL_INFO, " INT#: %u  ERR_CODE: 0x%08x\n", regs->int_no, regs->err_code);
+        KLOG(LOG_LEVEL_INFO, " EAX: 0x%x  EBX: 0x%x  ECX: 0x%x  EDX: 0x%x\n", regs->eax, regs->ebx, regs->ecx, regs->edx);
+        KLOG(LOG_LEVEL_INFO, " ESI: 0x%x  EDI: 0x%x  EBP: 0x%x  ESP: 0x%x\n", regs->esi, regs->edi, regs->ebp, regs->esp);
+        KLOG(LOG_LEVEL_INFO, " EIP: 0x%x  CS:  0x%x  EFLAGS: 0x%x\n", regs->eip, regs->cs, regs->eflags);
+        KLOG(LOG_LEVEL_INFO, " USERESP: 0x%x  SS: 0x%x\n", regs->useresp, regs->ss);
+        KLOG(LOG_LEVEL_INFO, " INT#: %u  ERR_CODE: 0x%x\n", regs->int_no, regs->err_code);
         KLOG(LOG_LEVEL_INFO, "========================\n");
     } else {
         KLOG(LOG_LEVEL_INFO, "No register state provided.\n");
